fad21caed0f0bc914d5658d36e0cebad8855620e
hsinpa <hsinpa@gmail.com>
2020-03-18 13:42:53 +0800

    1. Unirx 2. Switch between differnt texture type

---
+++ Assets/Hsinpa/VideoViewMeshSwitcher.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UniRx;
+using RenderHeads.Media.AVProVideo;
+
+
+public class VideoViewMeshSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    MeshFilter mesh;
+
+    [SerializeField]
+    Renderer renderer;
+
+    [SerializeField]
+    private MediaPlayer avplayer;
+
+    [SerializeField]
+    private ApplyToMesh applyToMesh;
+
+    [SerializeField]
+    private Mesh[] meshTypes;
+
+    [SerializeField]
+    private Material[] materialTypes;
+
+    private int currentIndex = 0;
+
+    private void Start()
+    {
+        SubscribeKeyboardEvent();
+        RotateViewType(currentIndex);
+    }
+
+    private void SubscribeKeyboardEvent() {
+
+        var keyboardObservable = Observable.EveryUpdate().Where(_ => Input.anyKeyDown);
+
+        keyboardObservable.Buffer(keyboardObservable.Throttle(System.TimeSpan.FromMilliseconds(250)))
+          .Where(xs => xs.Count >=1)
+          .Subscribe(xs =>
+          {
+              currentIndex = (currentIndex + 1) % meshTypes.Length;
+              RotateViewType(currentIndex);
+              Debug.Log("DoubleClick Detected! Count:" + xs.Count);
+          });
+    }
+
+    private void RotateViewType(int index) {
+        mesh.mesh = meshTypes[index];
+        renderer.material = materialTypes[index];
+
+        applyToMesh.enabled = false;
+
+        _ = Utility.UtilityMethod.DoDelayWork(0.1f, () => {
+            applyToMesh.enabled = true;
+        });
+    }
+
+}
